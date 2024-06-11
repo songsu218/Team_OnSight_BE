dba7d6d44ac982c1b4d0d6492a379644541f9f31
songsu218 <asane7@naver.com>
2024-06-11 17:01:49 +0900

    누락 추가

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const port = 8000;
 
 const express = require('express');
 const userController = require('./controllers/userController');
-const mongoose = require('mongoose');
+
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
 
@@ -16,17 +16,6 @@ app.use('/user', userController);
 
 app.get('/', function (req, res) {
   res.send('Hello World');
-});
-
-//회원가입 요청
-app.get('/register/:emailId/:nick/:password', async (req, res) => {
-  const { emailId, nick, password } = req.params;
-  res.send(`${emailId}, ${nick}, ${password}`);
-  console.log(emailId, nick, password);
-  try {
-    const userDoc = await User.create({});
-    console.log(userDoc);
-  } catch (e) {}
 });
 
 app.listen(port, () => {
