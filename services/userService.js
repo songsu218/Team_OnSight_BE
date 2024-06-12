9bed6bfad4b93913a950586c549b41741a34ecc0
songsu218 <asane7@naver.com>
2024-06-13 00:14:37 +0900

    feat: 일반 로그인 기능 개발

--- services/userService.js
+++ services/userService.js
@@ -1,17 +1,35 @@
 const User = require('../models/User');
 const hashUtils = require('../utils/hashUtils');
+const env = require('../config/env');
+const jwt = require('jsonwebtoken');
 
 async function register(userData) {
   const userDoc = await User.create({
-    emailId: encodeURIComponent(userData.emailId),
-    password: encodeURIComponent(hashUtils.hashPassword(userData.password)),
-    nick: encodeURIComponent(userData.nick),
+    id: userData.id,
+    password: await hashUtils.hashPassword(userData.password),
+    nick: userData.nick,
     thumbnail: null,
     crews: [],
   });
   return userDoc;
 }
 
+async function login(userData) {
+  const userDoc = await User.findOne({ id: userData.id });
+  if (!userDoc) {
+    return { message: 'nouser' };
+  }
+  const passOK = hashUtils.comparePassword(userData.password, userDoc.password);
+
+  if (passOK) {
+    const token = jwt.sign({ id: userDoc._id }, env.jwtSecret, {});
+    return { id: userDoc._id, token };
+  } else {
+    return { message: 'failed' };
+  }
+}
+
 module.exports = {
   register,
+  login,
 };
