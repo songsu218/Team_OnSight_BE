6b2511393d2dedaf0d016be650539de127227b58
kyuhwan <konge2963@gmail.com>
2024-06-15 00:30:48 +0900

    feat: main 백엔드

---
+++ models/Record.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+const RecordSchema = new Schema({
+  title: { type: String, required: true },
+  detail: { type: String, required: true },
+  center: { type: String, required: true },
+  date: { type: Date, required: true },
+  level: { type: Map, of: Number, required: true },
+  thumbnail: { type: String },
+});
+
+const Record = model('Record', RecordSchema);
+
+module.exports = Record;
