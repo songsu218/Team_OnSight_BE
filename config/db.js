5b94684ec4ea7828ca519a2b2d5aba8be39758e6
chaengleee <chaengleee@naver.com>
2024-06-19 19:33:48 +0900

    feat : debugger 추가

--- config/db.js
+++ config/db.js
@@ -1,11 +1,14 @@
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
+mongoose.set("debug", true);
 const connectUrl =
-  'mongodb+srv://sungu:sungu1111@cluster0.gxj08vx.mongodb.net/onsight?retryWrites=true&w=majority&appName=Cluster0';
+  "mongodb+srv://sungu:sungu1111@cluster0.gxj08vx.mongodb.net/onsight?retryWrites=true&w=majority&appName=Cluster0";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(connectUrl);
+    await mongoose.connect(connectUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
   } catch (err) {
     console.error(err.message);
   }
