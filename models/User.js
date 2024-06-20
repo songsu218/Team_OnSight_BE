398d05a1148d20d3527a7ac55f08124b8cb29b80
songsu218 <asane7@naver.com>
2024-06-20 16:55:21 +0900

    feat: 일반로그인과 회원가입 수정

--- models/User.js
+++ models/User.js
@@ -6,6 +6,10 @@ const UserSchema = new Schema({
   nick: { type: String, required: true },
   thumbnail: { type: String, default: '' },
   crews: { type: [String], default: [] },
+  events: { type: [String], default: [] },
+  like: { type: [String], default: [] },
+  recordcount: { type: Number, default: 0 },
+  feedcount: { type: Number, default: 0 },
 });
 
 const UserModel = model('User', UserSchema);
