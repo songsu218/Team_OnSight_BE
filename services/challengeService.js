c5cba867b3663da1430555a72b14d39df19d3e5c
이채연 <chaengleee@naver.com>
2024-06-23 19:28:26 +0900

    feat: 지저분한 소스 정리

--- services/challengeService.js
+++ services/challengeService.js
@@ -65,30 +65,19 @@ async function challengeMyList(ChallengeData) {
       }
     }
 
-    /* center Collection 생성되면 주석풀고 테스트 해보자
-    //thumbnail challenge에서 가져오는것으로 바뀜. 2024.06.23                                         
-    const centers = await Center.find();
-    records.aggregate([
-      {
-          $lookup: {
-            from: "center"
-            , localField: "center"
-            , foreignField: "center"
-            , as: "thumbnail"
-          }
-      }
-      ]);
-     */
-
     if (!records) {
       return { message: "no Challenges List" };
     }
 
     for (let index = 0; index < records.length; index++) {
       const date = records[index].date.slice();
+      const sdate = date[0];
+      const edate = date[1];
       records[index].state = retState(date);
       //front에서 바인딩 하기 쉽게 날짜 포멧팅
-      setDateString(records, index, date);
+      records[index].start_date = sdate.toISOString().split("T")[0];
+      records[index].end_date = edate.toISOString().split("T")[0];
+      records[index].date_string = records[index].start_date + " ~ " + records[index].end_date;      
     }
     return records;
   } catch (error) {
@@ -119,29 +108,18 @@ async function challengeTotList(ChallengeData) {
         .exec();
     }
 
-    /* center Collection 생성되면 주석풀고 테스트 해보자
-    //thumbnail challenge에서 가져오는것으로 바뀜. 2024.06.23                                          
-    const centers = await Center.find();
-    records.aggregate([
-      {
-          $lookup: {
-            from: "center"
-            , localField: "center"
-            , foreignField: "center"
-            , as: "thumbnail"
-          }
-      }
-      ]);
-     */
-
     if (!records) {
       return { message: "no Challenges List" };
     }
     for (let index = 0; index < records.length; index++) {
       const date = records[index].date.slice();
+      const sdate = date[0];
+      const edate = date[1];
       records[index].state = retState(date);
       //front에서 바인딩 하기 쉽게 날짜 포멧팅
-      setDateString(records, index, date);
+      records[index].start_date = sdate.toISOString().split("T")[0];
+      records[index].end_date = edate.toISOString().split("T")[0];
+      records[index].date_string = records[index].start_date + " ~ " + records[index].end_date;
     }
     return records;
   } catch (error) {
@@ -161,13 +139,18 @@ async function challengeMemberList(ChallengeData) {
       .sort({ _id: -1 })
       .lean()
       .exec();
+
+    if (!records) {
+      return { message: "no Challenges List" };
+    }
+  
     const usersList = [];
     //User에서 id,nick,thumbnail가져온다
     for (let index = 0; index < records.length; index++) {
       const members = records[index].members.slice();
       if (members != null && members.length > 0) {
         for (let k = 0; k < members.length; k++) {
-          console.log(members[k]);
+          //console.log(members[k]);
           const userDoc = await User.findOne({ id: members[k] });
           if (userDoc) {
             const challengeUser = {
@@ -183,8 +166,9 @@ async function challengeMemberList(ChallengeData) {
 
     return usersList;
   } catch (error) {
-    console.error("error", error);
-    throw error;
+    //console.error("error", error);
+    //throw error;
+    return { message: "challengeUser find mongoDB error" };
   }
 }
 
@@ -197,7 +181,6 @@ async function challengeRanking(ChallengeData) {
     );
     let levelList = null;
     if (challenge) {
-      //levelList = await challengeLevel(challenge.center);
       levelList = await Record.aggregate([
         {$match : { center : challenge.center }},
         {$group : {_id : "$userId", total : {$sum : "$levelsum"}}},
@@ -223,8 +206,9 @@ async function challengeRanking(ChallengeData) {
     }
     return levelList;
   } catch (error) {
-    console.error("error", error);
-    throw error;
+    //console.error("error", error);
+    //throw error;
+    return { message: "challengeRanking find mongoDB error" };
   }
 }
 
@@ -236,20 +220,6 @@ async function challengeInfo(ChallengeData) {
     })
       .lean()
       .exec();
-    /* center Collection 생성되면 주석풀고 테스트 해보자
-    //thumbnail challenge에서 가져오는것으로 바뀜. 2024.06.23                                          
-      const centers = await Center.find();
-      records.aggregate([
-        {
-            $lookup: {
-              from: "center"
-              , localField: "center"
-              , foreignField: "center"
-              , as: "thumbnail"
-            }
-        }
-        ]);
-      */
     if (!records) {
       return { message: "no Challenges List" };
     }    
@@ -260,88 +230,6 @@ async function challengeInfo(ChallengeData) {
     return { message: "Challenges find mongoDB error" };
   }
 }
-
-//center,id별로 levelcount 합산
-async function challengeLevel(center) {
-  try {
-    let temp = null;
-    let result = null;
-    let records = await Record.find({ center: center }).lean().exec();
-    for (let index = 0; index < records.length; index++) {
-      const element = levelCalc(records[index].level);
-      //const element = levelCalc(records[index].level.slice());
-      records[index].total = element;
-    }
-
-    //id별로 Group by sum
-    /**  이부분 오류있어 주석하였습니다. */
-    // if(records.length > 0){
-
-    //   temp = records.reduce(function(res, obj) {
-    //   if (!(obj.id in res))
-    //       //res.push(res[obj.id] = obj);
-    //     res.push(obj);
-    //   else {
-    //       res[obj.id].total += obj.total;
-    //   }
-    //   return temp;
-    //  });
-
-    //  //result = temp.sort(arrOrderDesc("total"));
-    //  //result = sortByKeyDesc(temp, 'total');
-    //  result = records; //임시
-    // }
-    result = records;
-    return result;
-  } catch (error) {
-    console.error("error", error);
-    throw error;
-  }
-}
-
-//level+점수 합산
-function levelCalc(level) {
-  let temp = 0;
-  let sum = 0;
-  for (const key in level) {
-    if (level.hasOwnProperty(key)) {
-      //console.log(`${key} ${level[key]}`);
-      temp = Number(key) * Number(level[key]);
-      sum = sum + temp;
-    }
-  }
-  /*
-  for(let k=0;k<level.length;k++){
-    const val1 = arLevel[k][0];
-    const val2 = arLevel[k][1];
-    temp = Number(val1) * Number(val2);
-    sum = sum + temp;
-  }
-  */
-  return sum;
-}
-
-//순위정렬(내림차수)
-function arrOrderDesc(key) {
-  return function (a, b) {
-    if (b[key] > a[key]) {
-      return 1;
-    } else if (b[key] < a[key]) {
-      return -1;
-    }
-
-    return 0;
-  };
-}
-// 특정 키를 기준으로 내림차순 정렬하는 함수
-function sortByKeyDesc(array, key) {
-  return array.sort((a, b) => {
-    if (a[key] > b[key]) return -1;
-    if (a[key] < b[key]) return 1;
-    return 0;
-  });
-}
-
 
 //종료일을 기준으로 상태(NOW,PAST)를 return 한다
 function retState(date) {
@@ -356,17 +244,6 @@ function retState(date) {
 
   return str;
 }
-
-function setDateString(records, index, date) {
-  const sdate = date[0];
-  const edate = date[1];
-
-  records[index].start_date = sdate.toISOString().split("T")[0];
-  records[index].end_date = edate.toISOString().split("T")[0];
-  records[index].date_string = records[index].start_date + " ~ " + records[index].end_date;
-
- }
-
 
 // 유저의 events 배열 안의 _id와 맞는 챌린지리스트 조회 - 송성우
 async function userChallengeList(userData) {
@@ -390,6 +267,5 @@ module.exports = {
   challengeMemberList,
   challengeInfo,
   challengeRanking,
-  challengeLevel,
   userChallengeList,
 };
